81ee4a20ef8bae708397f7bd8f47f7d75a8052a8
Sujeet76 <126092124+Sujeet76@users.noreply.github.com>
2025-01-03 20:56:04 +0530

    chore: add conventional-changelog-atom dependency

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,4 @@
-import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity, type UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
@@ -6,23 +6,24 @@ const Configuration: UserConfig = {
   formatter: "@commitlint/format",
   rules: {
     "type-enum": [
-      2,
+      RuleConfigSeverity.Error,
       "always",
       [
-        "feat ✨", // New feature
-        "fix 🐛", // Bug fix
-        "docs 📝", // Documentation changes
-        "style 💎", // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
-        "refactor ♻️", // Code changes that neither fix a bug nor add a feature
-        "perf ⚡️", // Performance improvement
-        "test ✅", // Adding missing tests or correcting existing tests
-        "build 🏗️", // Changes that affect the build system or external dependencies (example scopes: npm)
-        "ci 🛠️", // Changes to CI configuration files and scripts
-        "chore 📦", // Other changes that don't modify src or test files
-        "revert ⏪", // Reverts a previous commit
-        "assets 🖼️", // Changes related to assets
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        "refactor", // Code changes that neither fix a bug nor add a feature
+        "perf", // Performance improvement
+        "test", // Adding missing tests or correcting existing tests
+        "build", // Changes that affect the build system or external dependencies (example scopes: npm)
+        "ci", // Changes to CI configuration files and scripts
+        "chore", // Other changes that don't modify src or test files
+        "revert", // Reverts a previous commit
+        "assets", // Changes related to assets
       ],
     ],
+    "header-max-length": [RuleConfigSeverity.Disabled, "always", 150],
   },
 };
 
